b8a14b410e28117575c7c81c8527718574bdf911
Alexis-Miras <alexisof77@gmail.com>
2025-10-22 17:44:31 +0200

    Ration & read input.txt

--- TD2/skeleton_ex/skeleton_ex1/main.c
+++ TD2/skeleton_ex/skeleton_ex1/main.c
@@ -4,7 +4,6 @@
 #include "search.h"
 #include "utils.h"
 
-    /*
 int cmpfunc(const void *a, const void *b) {
     int ia = *(int*)a;
     int ib = *(int*)b;
@@ -12,35 +11,39 @@ int cmpfunc(const void *a, const void *b) {
     else if (ia > ib) return 1;
     else return 0;
 }
-    */
 
 int main(void) {
+
+    FILE *f = fopen("../input.txt", "r");
+    if (!f) {
+        perror("Erreur d'ouverture du fichier");
+        return 1;
+    }
+
     int n = 1000;
     int target = 5629;
     int* sort_arr = malloc(sizeof(int)*n);
     int* unsort_arr = malloc(sizeof(int)*n);
 
-    srand(time(NULL));
-
     for (int i = 0; i < n; i++) {
-        sort_arr[i] = rand() % 10000;
-        unsort_arr[i] = rand() % 10000;
+        if (fscanf(f, "%d", &sort_arr[i]) != 1) {
+            fprintf(stderr, "Erreur de lecture à la ligne %d\n", i + 1);
+            fclose(f);
+            return EXIT_FAILURE;
+        }
+        if (fscanf(f, "%d", &unsort_arr[i]) != 1) {
+            fprintf(stderr, "Erreur de lecture à la ligne %d\n", i + 1);
+            fclose(f);
+            return EXIT_FAILURE;
+        }
     }
+    fclose(f);
 
     int idx = rand() % n;
     sort_arr[idx] = target;
     unsort_arr[idx] = target;
 
-    // qsort(sort_arr, n, sizeof(int), cmpfunc);
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (sort_arr[i] > sort_arr[j]) {
-                int tmp = sort_arr[i];
-                sort_arr[i] = sort_arr[j];
-                sort_arr[j] = tmp;
-            }
-        }
-    }
+    qsort(sort_arr, n, sizeof(int), cmpfunc);
 
     clock_t starting_clock_linear = clock();
     int linear_res = linear_search(unsort_arr, n, target);
@@ -64,6 +67,10 @@ int main(void) {
 
         printf("Jump search (%i) = %i, time : %f seconds \n", target, jump_res, jump_time);
         printf("Binary search (%i) = %i, time : %f seconds \n", target, binary_res, binary_time);
+        
+        printf("\nRatios of execution times:\n");
+        printf("- Linear/Jump: %f\n", linear_time / jump_time);
+        printf("- Binary/Jump: %f\n", binary_time / jump_time);
             
     } else {
         printf("Unsorted array \n");
--- TD2/skeleton_ex/skeleton_ex2/main.c
+++ TD2/skeleton_ex/skeleton_ex2/main.c
@@ -4,42 +4,58 @@
 #include "sort.h"
 #include "utils.h"
 
+#include <string.h>
+
 int main(void) {
-    int n = 25000;
-    int* arr1 = malloc(sizeof(int)*n);
-    int* arr2 = malloc(sizeof(int)*n);
-    int* arr3 = malloc(sizeof(int)*n);
-    int* arr4 = malloc(sizeof(int)*n);
-    int* arr5 = malloc(sizeof(int)*n);
 
-    srand(time(NULL));
-
-    for (int i = 0; i < n; i++) {
-        arr1[i] = rand() % 10000;
-        arr2[i] = rand() % 10000;
-        arr3[i] = rand() % 10000;
-        arr4[i] = rand() % 10000;
-        arr5[i] = rand() % 10000;
+    FILE *f = fopen("../input.txt", "r");
+    if (!f) {
+        perror("Erreur d'ouverture du fichier");
+        return 1;
     }
 
+    int n = 25000;
+    int* read_arr = malloc(sizeof(int)*n);
+    int* selection_arr = malloc(sizeof(int)*n);
+    int* insertion_arr = malloc(sizeof(int)*n);
+    int* bubble_arr = malloc(sizeof(int)*n);
+    int* merge_arr = malloc(sizeof(int)*n);
+    int* quick_arr = malloc(sizeof(int)*n);
+
+    for (int i = 0; i < n; i++) {
+        if (fscanf(f, "%d", &read_arr[i]) != 1) {
+            fprintf(stderr, "Erreur de lecture à la ligne %d\n", i + 1);
+            fclose(f);
+            return EXIT_FAILURE;
+        }
+    }
+    fclose(f);
+
+    memcpy(selection_arr, read_arr, n * sizeof(int));
+    memcpy(insertion_arr, read_arr, n * sizeof(int));
+    memcpy(bubble_arr, read_arr, n * sizeof(int));
+    memcpy(merge_arr, read_arr, n * sizeof(int));
+    memcpy(quick_arr, read_arr, n * sizeof(int));
+
+
     clock_t starting_clock_selection = clock();
-    selection_sort(arr1, n);
+    selection_sort(selection_arr, n);
     clock_t ending_clock_selection = clock();
 
     clock_t starting_clock_insertion = clock();
-    insertion_sort(arr2, n);
+    insertion_sort(insertion_arr, n);
     clock_t ending_clock_insertion = clock();
 
     clock_t starting_clock_bubble = clock();
-    bubble_sort(arr3, n);
+    bubble_sort(bubble_arr, n);
     clock_t ending_clock_bubble = clock();
 
     clock_t starting_clock_merge = clock();
-    merge_sort(arr4, n);
+    merge_sort(merge_arr, n);
     clock_t ending_clock_merge = clock();
 
     clock_t starting_clock_quick = clock();
-    quick_sort(arr5, n);
+    quick_sort(quick_arr, n);
     clock_t ending_clock_quick = clock();
 
     double selection_time = (double)(ending_clock_selection - starting_clock_selection) / CLOCKS_PER_SEC;
@@ -48,18 +64,52 @@ int main(void) {
     double merge_time = (double)(ending_clock_merge - starting_clock_merge) / CLOCKS_PER_SEC;
     double quick_time = (double)(ending_clock_quick - starting_clock_quick) / CLOCKS_PER_SEC;
 
-    printf("Sorting %i random values:\n", n);
-    printf("Selection sort time: %f seconds \n", selection_time);
-    printf("Insertion sort time: %f seconds \n", insertion_time);
-    printf("Bubble sort time: %f seconds \n", bubble_time);
-    printf("Merge sort time: %f seconds \n", merge_time);
-    printf("Quick sort time: %f seconds \n", quick_time);
+    FILE *f_out_s = fopen("output_selection.txt", "w");
+    FILE *f_out_i = fopen("output_insertion.txt", "w");
+    FILE *f_out_b = fopen("output_bubble.txt", "w");
+    FILE *f_out_m = fopen("output_merge.txt", "w");
+    FILE *f_out_q = fopen("output_quick.txt", "w");
 
-    free(arr1);
-    free(arr2);
-    free(arr3);
-    free(arr4);
-    free(arr5);
+    if (!f_out_i || !f_out_s || !f_out_b || !f_out_m || !f_out_q) {
+        perror("Erreur ouverture output.txt");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < n; i++) {
+        fprintf(f_out_s, "%d\n", selection_arr[i]);
+        fprintf(f_out_i, "%d\n", insertion_arr[i]);
+        fprintf(f_out_b, "%d\n", bubble_arr[i]);
+        fprintf(f_out_m, "%d\n", merge_arr[i]);
+        fprintf(f_out_q, "%d\n", quick_arr[i]);
+    }
+
+    fclose(f_out_s);
+    fclose(f_out_i);
+    fclose(f_out_b);
+    fclose(f_out_m);
+    fclose(f_out_q);
+
+    free(read_arr);
+    free(selection_arr);
+    free(insertion_arr);
+    free(bubble_arr);
+    free(merge_arr);
+    free(quick_arr);
+
+
+    printf("Sorting %i random values:\n", n);
+    printf("- Selection sort time: %f seconds \n", selection_time);
+    printf("- Insertion sort time: %f seconds \n", insertion_time);
+    printf("- Bubble sort time: %f seconds \n", bubble_time);
+    printf("- Merge sort time: %f seconds \n", merge_time);
+    printf("- Quick sort time: %f seconds \n", quick_time);
+
+    printf("\nRatios of execution times:\n");
+    printf("- Selection/Bubble: %f\n", selection_time / bubble_time);
+    printf("- Insertion/Bubble: %f\n", insertion_time / bubble_time);
+    printf("- Merge/Bubble: %f\n", merge_time / bubble_time);
+    printf("- Quick/Bubble: %f\n", quick_time / bubble_time);
+
 
     return 0;
 }
